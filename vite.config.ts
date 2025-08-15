2c4901eed0720125b6a60ceff702ab8390e80d89
glebv <glebusheg@gmail.com>
2025-08-16 01:13:28 +0200

    Basic setup

---
+++ vite.config.ts
@@ -0,0 +1,34 @@
+import { advzipPlugin, defaultAdvzipOptions, defaultEctOptions, ectPlugin } from "js13k-vite-plugins";
+import { defineConfig } from "vite";
+import { createHtmlPlugin } from "vite-plugin-html";
+import { viteSingleFile } from "vite-plugin-singlefile";
+
+export default defineConfig({
+  base: "./",
+  plugins: [
+    viteSingleFile({ useRecommendedBuildConfig: false, removeViteModuleLoader: true }),
+    createHtmlPlugin({ minify: true }),
+    ectPlugin(defaultEctOptions),
+    advzipPlugin(defaultAdvzipOptions),
+    {
+      name: "final-transformations",
+      enforce: "post",
+      renderChunk: async (code: string) => {
+        return {
+          code: code.replaceAll("const ", "let "),
+          map: null,
+        };
+      },
+    },
+  ],
+  build: {
+    assetsDir: ".",
+    assetsInlineLimit: 0,
+    cssCodeSplit: false,
+    rollupOptions: {
+      output: {
+        assetFileNames: "[name].[ext]", // Keep original asset names
+      },
+    },
+  },
+});
