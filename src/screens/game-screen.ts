47202a02ba6643a29583133217fa408c99ec6590
glebv <glebusheg@gmail.com>
2025-09-11 22:45:53 +0200

    Add conditional dat.GUI for debug

--- src/screens/game-screen.ts
+++ src/screens/game-screen.ts
@@ -11,9 +11,21 @@ export class GameScreen extends BaseScreen {
     this.title = new Text("Game Screen", 64, "Arial", "bold");
 
     this.add(this.title);
+
+    if (import.meta.env.DEV) {
+      import("dat.gui").then((dat) => {
+        const gui = new dat.GUI();
+        const gameFolder = gui.addFolder("GameScreen");
+        gameFolder.open();
+      });
+    }
   }
 
   override doResize(): void {
     this.title.setPosition(c.width / 2, c.height / 2);
+
+    console.log("GameScreen doResize");
   }
 }
+
+if (import.meta.hot) import.meta.hot.accept();
